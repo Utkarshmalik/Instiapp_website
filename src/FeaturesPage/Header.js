4dfefb764bbd2b1bee58f3776d9c617ea2d48ac9
Utkarsh Malik <utkarshmalik06@gmail.com>
2021-08-07 23:57:28 +0530

    Update Header.js

--- src/FeaturesPage/Header.js
+++ src/FeaturesPage/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MainMenu from '../components/MainMenu';
 const HeaderSection = () => (
   <section className="main-menu-section">
-    <MainMenu fixedMenuTopHeight="200" />
+    <MainMenu fixedMenuTopHeight="300" />
   </section>
 );
 export default HeaderSection;
